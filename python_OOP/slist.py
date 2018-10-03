0a9e588b8df1df496f32a331ced935293807a7e0
Chris Truax <c2truax@gmail.com>
2018-10-02 22:35:58 -0500

    another commit

---
+++ python_OOP/slist.py
@@ -0,0 +1,10 @@
+#Part 1 - recreate SList and Node.  Recreate addNode and printAllValues methods.
+class Node:
+    def __init__(self,value):
+        self.value = value
+        self.next = none
+
+class SList:
+    def __init__(self,head):
+        self.head = none
+    def addNode(self,)
