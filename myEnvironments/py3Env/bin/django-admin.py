644237b0969b8190536deb992713fd1a8b614243
Chris Truax <c2truax@gmail.com>
2018-11-05 17:23:12 -0600

    commit nov 5

---
+++ myEnvironments/py3Env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/christophertruax/Desktop/Coding Dojo/python_stack/myEnvironments/py3Env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
